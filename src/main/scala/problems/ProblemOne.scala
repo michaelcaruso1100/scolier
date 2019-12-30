db7c28513c279ed5bd0124ba80a05a252f346ae6
Mike Caruso <wintermadman1100@gmail.com>
2019-12-29 21:24:03 -0500

    FInished PRoblem 1.Started problem 2

--- src/main/scala/problems/ProblemOne.scala
+++ src/main/scala/problems/ProblemOne.scala
@@ -9,5 +9,20 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 package problems
 
 object ProblemOne extends App {
-  println("Test")
+
+  val limit = 1 to 999 
+  
+  def f (num : Int) : Boolean = {
+	num match {
+	
+		case num if num % 5 ==0 => true
+		case num if num % 3 ==0 => true
+		case _ => false
+	}
+
+      }
+
+  val filtered =  limit.filter(f)
+  println(filtered)
+  println(filtered.sum)
 }
